78db19edfab30d14e0d0dffc26bf15b513372056
nadaf-reshma <49471505+nadaf-reshma@users.noreply.github.com>
2019-04-10 16:55:58 +0530

    Create test.c

---
+++ test.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int number=5;
+    if(number % 2 == 0)
+        printf("%d is even.", number);
+    else
+        printf("%d is odd.", number);
+
+    return 0;
+}
